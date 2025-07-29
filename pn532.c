33cdb7b35b003de0bd164142de573fd7db839542
allan888 <admin@crawlingcrab.cn>
2025-07-29 23:05:18 +0800

    实现了正确的唤醒命令

--- pn532.c
+++ pn532.c
@@ -8,11 +8,26 @@ result build_inlistpassivetarget_command() {
     int payload_len = sizeof(payload);
     return build_template(payload,payload_len);
 }
-result build_sam_configuration_command() {
+result build_sam_configuration_command() { //生成唤醒命令
     // 指令字段（包含 TFI）
     uint8_t payload[] = {0xD4,0x14,0x01};
     int payload_len = sizeof(payload);
-    return build_template(payload,payload_len);
+    result r =  build_template(payload,payload_len);
+    static uint8_t final_payload[24]={0};
+    for(int i=0;i<2;i++){
+        final_payload[i] = 0x55;
+    }
+    for(int i=2;i<14;i++){
+        final_payload[i] = 0x00;
+    }
+    for(int i=14;i<24;i++){
+        final_payload[i] = r.data[i-14];
+    }
+    result final;
+    final.len = 24;
+    final.data = final_payload;
+    free(r.data);
+    return final;
 }
 result build_authenticate_keyA(const uint8_t* uid,const uint8_t* keyA,uint8_t block){
     uint8_t payload[15]={0xD4,0x40,0x01,0x60};
@@ -41,7 +56,7 @@ result build_read_card(uint8_t block){
     payload[4] = block;
     return build_template(payload,5);
 }
-result build_template(uint8_t* payload,int payload_len) {
+result build_template(const uint8_t* payload,int payload_len) {
     result r;
     r.len = 7 + payload_len; // 00 FF LEN LCS ... DCS 00
     r.data = (uint8_t *)malloc(r.len);
--- pn532.h
+++ pn532.h
@@ -12,7 +12,7 @@ typedef struct {
 } result;
 result build_inlistpassivetarget_command();
 result build_sam_configuration_command();
-result build_template(uint8_t* payload,int payload_len);
+result build_template(const uint8_t* payload,int payload_len);
 result build_authenticate_keyA(const uint8_t* uid,const uint8_t* keyA,uint8_t block);
 result build_authenticate_keyB(const uint8_t* uid,const uint8_t* keyB,uint8_t block);
 result build_read_card(uint8_t block);

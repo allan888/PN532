2308d6844b4d0dc9894189a6840a749a2b71f3b5
allan888 <admin@crawlingcrab.cn>
2025-08-01 12:16:26 +0800

    优化了逻辑，添加了将命令写入剪贴板中的功能。

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define RESET   "\033[0m"
 #include <stdio.h>
 #include "pn532.h"
+#include <windows.h>
+int CopyIntoClipboard(result r);
 int main(){
     printf("\033[38;2;57;197;187m");
     printf("欢迎来到自动生成pn532串口命令小工具\n");
@@ -18,26 +20,31 @@ int main(){
         int command = 0;
         int len = 0;
         uint8_t block = 0;
+        result r;
         printf("请输入命令：");
         scanf("%d",&command);
         switch(command){
             case 1:
-                len =  build_sam_configuration_command().len;
+                r = build_sam_configuration_command();
+                len =  r.len;
                 printf(BLUE);
                 for(int i=0;i<len;i++){
-                    if(i!=(len - 1)) printf("%02X ",build_sam_configuration_command().data[i]);
-                    else printf("%02X\n",build_sam_configuration_command().data[i]);
+                    if(i!=(len - 1)) printf("%02X ",r.data[i]);
+                    else printf("%02X\n",r.data[i]);
                 }
                 printf(RESET);
+                CopyIntoClipboard(r);
                 break;
             case 2:
                 printf(BLUE);
-                len =  build_inlistpassivetarget_command().len;
+                r = build_inlistpassivetarget_command();
+                len =  r.len;
                 for(int i=0;i<len;i++){
-                    if(i!=(len-1)) printf("%02X ",build_inlistpassivetarget_command().data[i]);
-                    else printf("%02X\n",build_inlistpassivetarget_command().data[i]);
+                    if(i!=(len-1)) printf("%02X ",r.data[i]);
+                    else printf("%02X\n",r.data[i]);
                 }
                 printf(RESET);
+                CopyIntoClipboard(r);
                 break;
             case 3:
                 const uint8_t uid[4];
@@ -47,35 +54,79 @@ int main(){
                 scanf("%hhX %hhx %hhx %hhx",&uid[0],&uid[1],&uid[2],&uid[3]);
                 printf("请输入keyA：");
                 scanf("%hhX %hhx %hhx %hhx %hhx %hhx",&keyA[0],&keyA[1],&keyA[2],&keyA[3],&keyA[4],&keyA[5]);
-                printf("请输入keyB：");
-                scanf("%hhX %hhx %hhx %hhx %hhx %hhx",&keyB[0],&keyB[1],&keyB[2],&keyB[3],&keyB[4],&keyB[5]);
                 printf("请输入block：");
                 scanf("%hhx",&block);
+                r = build_authenticate_keyA(uid,keyA,block);
                 printf(BLUE);
-                len = build_authenticate_keyA(uid,keyA,block).len;
+                len = r.len;
                 for(int i=0;i<len;i++){
-                    if(i!=(len-1)) printf("%02X ",build_authenticate_keyA(uid,keyA,block).data[i]);
-                    else printf("%02X\n",build_authenticate_keyA(uid,keyA,block).data[i]);
+                    if(i!=(len-1)) printf("%02X ",r.data[i]);
+                    else printf("%02X\n",r.data[i]);
                 }
-                len = build_authenticate_keyB(uid,keyB,0x00).len;
+                CopyIntoClipboard(r);
+                printf("请输入keyB：");
+                scanf("%hhX %hhx %hhx %hhx %hhx %hhx",&keyB[0],&keyB[1],&keyB[2],&keyB[3],&keyB[4],&keyB[5]);
+                r = build_authenticate_keyB(uid,keyB,block);
+                len = r.len;
                 for(int i=0;i<len;i++){
-                    if(i!=(len-1)) printf("%02X ",build_authenticate_keyB(uid,keyB,block).data[i]);
-                    else printf("%02X\n",build_authenticate_keyB(uid,keyB,block).data[i]);
+                    if(i!=(len-1)) printf("%02X ",r.data[i]);
+                    else printf("%02X\n",r.data[i]);
                 }
                 printf(RESET);
+                CopyIntoClipboard(r);
                 break;
             case 4:
                 printf("请输入block：");
                 scanf("%hhx",&block);
-                len = build_read_card(block).len;
+                r = build_read_card(block);
+                len = r.len;
                 printf(BLUE);
                 for(int i=0;i<len;i++){
-                    if(i!=(len-1)) printf("%02X ",build_read_card(block).data[i]);
-                    else printf("%02X\n",build_read_card(block).data[i]);
+                    if(i!=(len-1)) printf("%02X ",r.data[i]);
+                    else printf("%02X\n",r.data[i]);
                 }
                 printf(RESET);
+                CopyIntoClipboard(r);
                 break;
         }
     }
+    return 0;
+}
+int CopyIntoClipboard(result r){
+    int len = r.len;
+    char temp[8];
+    char final[512] = {0};  // 最终字符串缓冲区
+
+    for(int i = 0; i < len; i++){
+        if(i != (len - 1))
+            sprintf(temp, "%02X ", r.data[i]);
+        else
+            sprintf(temp, "%02X\n", r.data[i]);
+
+        strcat(final, temp);  // 累加内容
+    }
+
+    size_t length = strlen(final) + 1;
+
+    if(!OpenClipboard(nullptr)){
+        printf(RED "未打开剪贴板\n" RESET);
+        return -1;
+    }
+
+    EmptyClipboard();
+
+    HGLOBAL hem = GlobalAlloc(GMEM_MOVEABLE, length);
+    if(!hem){
+        CloseClipboard();
+        printf(RED "未能分配全局内存\n" RESET);
+        return -1;
+    }
+
+    memcpy(GlobalLock(hem), final, length);
+    GlobalUnlock(hem);
+    SetClipboardData(CF_TEXT, hem);
+    CloseClipboard();
+
+    printf("成功写入剪贴板：%s", final);
     return 0;
 }
